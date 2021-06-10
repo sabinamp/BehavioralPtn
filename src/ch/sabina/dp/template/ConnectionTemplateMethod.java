cd543a90123f2798270fcdf56e6e5ffad3ed8f20
Sabina Maria <p_sabinamaria@yahoo.com>
2021-06-10 15:45:56 +0200

    add img

---
+++ src/ch/sabina/dp/template/ConnectionTemplateMethod.java
@@ -0,0 +1,37 @@
+package ch.sabina.dp.template;
+
+//defines abstract operations that concrete subclasses define to implement steps of an algorithm
+public abstract class ConnectionTemplateMethod {
+	public final void run() {
+		setDBDriver();
+		setCredentials();
+		connect();
+		prepareStatement();
+		setData();
+		insert();
+		close();
+		destroy();
+	}
+	
+	public abstract void setDBDriver();
+	public abstract void setCredentials();
+	
+	public void connect() {
+		System.out.println("Setting connection...");
+	}
+	public void prepareStatement() {
+		System.out.println("Preparing insert statement...");
+	}
+	public abstract void setData();
+	
+	public void insert() {
+		System.out.println("Inserting data...");
+	}
+	public void close() {
+		System.out.println("Closing connections...");
+	}
+		public void destroy() {
+		System.out.println("Destroying connection objects...");
+		}
+
+}
